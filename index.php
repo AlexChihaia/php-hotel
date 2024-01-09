1a1ae448cbe3bf362d748ec891af908478b4dd4a
AlexChihaia <alexchihaia84@gmail.com>
2024-01-09 17:34:33 +0100

    feat : bootstrap table

--- index.php
+++ index.php
@@ -46,19 +46,48 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotels</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-
 <h1>PHP Hotels</h1>
 
 <?php foreach ($hotels as $hotel) {
-        echo '<h2>' . $hotel['name'] . '</h2>';
-        echo '<p>Description: ' . $hotel['description'] . '</p>';
-        echo '<p>Parking: ' . ($hotel['parking'] ? 'Yes' : 'No') . '</p>';
-        echo '<p>Vote: ' . $hotel['vote'] . '</p>';
-        echo '<p>Distance to center: ' . $hotel['distance_to_center'] . ' ' . 'km' . '</p>';
-        echo '<hr>';
-    } ?>
+    # Stampa a schermo semplice dei dati
+
+#    echo '<h2>' . $hotel['name'] . '</h2>';
+#    echo '<p>Description: ' . $hotel['description'] . '</p>';
+#    echo '<p>Parking: ' . ($hotel['parking'] ? 'Yes' : 'No') . '</p>';
+#    echo '<p>Vote: ' . $hotel['vote'] . '</p>';
+#    echo '<p>Distance to center: ' . $hotel['distance_to_center'] . ' ' . 'km' . '</p>';
+#    echo '<hr>';
+} ?> 
+
+
+<!-- Stampa con tabella bootstrap -->
+<table class="table">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Parking</th>
+            <th>Vote</th>
+            <th>Distance to center</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($hotels as $hotel) {
+            echo '<tr>';
+            echo '<td>' . $hotel['name'] . '</td>';
+            echo '<td>' . $hotel['description'] . '</td>';
+            echo '<td>' . ($hotel['parking'] ? 'Yes' : 'No') . '</td>';
+            echo '<td>' . $hotel['vote'] . '</td>';
+            echo '<td>' . $hotel['distance_to_center'] . ' ' . 'km' . '</td>';
+            echo '</tr>';
+        } ?>
+    </tbody>
+</table>
+    
     
 </body>
 </html>
